0f0fed17e54dbbc078a9c3bf8678f3ba60fe77c3
Andy Mardell <andy@mardell.me>
2016-11-03 16:20:31 +0000

    Add 12x2 display support, add screen switching

--- hamster-1.0.2.cpp
+++ hamster-1.0.2.cpp
@@ -18,13 +18,15 @@
 #include <LiquidCrystal_I2C.h>
 
 // Setup LCD
-LiquidCrystal_I2C lcd(0x27, 20, 4);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 // Variables
 volatile byte revolutions;
 unsigned int rpm;
 unsigned long timeold;
 const int hallPin = 5; // Hall Effect Sensor on Pin D1
+const int buttonPin = 4;
+const int screen = 1; // Screen 1 or 2
 
 // Debugging
 const int debugging = 1;
@@ -39,12 +41,11 @@ void setup() {
   // Attach interupt to hallPin, call magnetDetect()
   attachInterrupt(hallPin, magnetDetect, RISING);
 
-  // Set up LCD for main program
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("RPM:");
-  lcd.setCursor(0, 1);
-  lcd.print("Revs:");
+  // Attach interupt to button, call changeDisplay()
+  attachInterrupt(buttonPin, changeDisplay, RISING);
+
+  // Set up display one
+  setupDisplayOne();
 
   // Reset vars
   revolutions = 0;
@@ -68,9 +69,14 @@ void loop() {
     }
     
   }
-  
+
   // Update Display
-  updateDisplay();
+  if (screen == 1) {
+    updateDisplayOne();  
+  } else {
+    updateDisplayTwo();
+  }
+  
   
 }
 
@@ -85,19 +91,85 @@ void magnetDetect() {
   
 }
 
-// Update display
-void updateDisplay() {
+// Change Display
+void changeDisplay() {
 
-  if (debugging) {
-    Serial.println("Function Called: updateDisplay()");
+  if (screen == 1) {
+    setupDisplayTwo();
+    screen = 2;
+  } else {
+    setupDisplayOne();
+    screen = 1;
   }
 
-  // Update RPM
-  lcd.setCursor(6, 0);
-  lcd.print(rpm);
+}
 
-  // Update Revolutions
-  lcd.setCursor(7, 1);
-  lcd.print(revolutions);
+// Setup display one
+void setupDisplayOne() {
+
+  if (debugging) {
+    Serial.println("Function Called: setupDisplayOne()");
+  }
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Speed     :");
+  lcd.setCursor(0, 1);
+  lcd.print("Top Speed :");
+  lcd.setCursor(13, 0);
+  lcd.print("mph");
+  lcd.setCursor(13, 1);
+  lcd.print("mph");
+}
+
+// Setup display two
+void setupDisplayTwo() {
+
+  if (debugging) {
+    Serial.println("Function Called: setupDisplayTwo()");
+  }
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Last Trip :");
+  lcd.setCursor(0, 1);
+  lcd.print("Total Dist:");
+  lcd.setCursor(14, 0);
+  lcd.print("mi");
+  lcd.setCursor(14, 1);
+  lcd.print("mi");
+}
+
+// Update display one
+void updateDisplayOne() {
+
+  if (debugging) {
+    Serial.println("Function Called: updateDisplayOne()");
+  }
+
+  // Update Speed
+  lcd.setCursor(12, 0);
+  lcd.print('0');
+
+  // Update Top Speed
+  lcd.setCursor(12, 1);
+  lcd.print('0');
+  
+}
+
+// Update display two
+void updateDisplayOne() {
+
+  if (debugging) {
+    Serial.println("Function Called: updateDisplayOne()");
+  }
+
+  // Update Last Trip
+  lcd.setCursor(13, 0);
+  lcd.print('0');
+
+  // Update Total Dist
+  lcd.setCursor(13, 1);
+  lcd.print('0');
   
 }
